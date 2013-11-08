213419c047456346ccaf739fc2f978083043dcf6
Barron Henderson <barronh@gmail.com>
2013-11-08 12:55:46 -0500

    Cleaning up and relaxing test-case

--- test/check.py
+++ test/check.py
@@ -9,23 +9,37 @@ def readfile(path):
     return data
 
 def check(check_path, test_path):
+    from math import copysign
     spec_ref = readfile(check_path)
     spec_test = readfile(test_path)
     
     abs_tol = 1e-5
-    rel_tol = 1e-5
-    print 'Species'.ljust(16) + ' Pass'
+    rel_tol = 1e-4
+    print 'Pass  ' + 'Species'.ljust(16)
     checks = []
     for key, refvals in spec_ref.iteritems():
         if key in spec_test:
             checkvals = spec_test[key]
             diffs = [checkval - refval for checkval, refval in zip(checkvals, refvals)]
             allowables = [abs(refval) * rel_tol + abs_tol for refval in refvals]
-            check = all([diff <= allowable for diff, allowable in zip(map(abs, diffs), allowables)])
+            tests = [diff <= allowable for diff, allowable in zip(map(abs, diffs), allowables)]
+            check = all(tests)
         else:
             check = 0
         checks.append(check)
         print str(check).ljust(5), key.ljust(16)
+        if check == False:
+            ti = 0
+            print 'Faild , Total , Pct', len(refvals) - sum(tests), ',', len(refvals), ',', round((1-sum(tests)/float(len(refvals)))*100, 1), '%'
+            for r, c, d, a, t in zip(refvals, checkvals, diffs, allowables, tests):
+                if not t:
+                    ti += 1
+                    print '  Fail:', ti
+                    print '  Ref    :', r
+                    print '  Check  :', c
+                    print '  Diff   :', d
+                    print '  Allowed:', copysign(a, d)
+                    print
     
     sum_checks = sum(checks)
     len_checks = len(checks)
@@ -34,8 +48,10 @@ def check(check_path, test_path):
     
     if len_checks > sum_checks:
         print '!!!!Failed:', len_checks - sum_checks
+    else:
+        print ''    
+    print ''    
     
-    assert(all(checks))
     return checks
 
 
