a283075fd90c6ac5a0eb8d7bd07b935ddebe4071
barronh <barronh@gmail.com>
2017-04-07 18:26:48 -0400

    Add python3 compatibility to check.py
    mostly fixing print statements

--- test/check.py
+++ test/check.py
@@ -1,6 +1,7 @@
+from __future__ import print_function
 endc = '\033[0m'
 def readfile(path):
-    lines = file(path, 'r').read().strip().split('\n')
+    lines = open(path, 'r').read().strip().split('\n')
     header = lines.pop(0)
     header = map(str.strip, header.split('!')[:-1])
     data = dict([(h, []) for h in header])
@@ -16,9 +17,9 @@ def check(check_path, test_path):
     
     abs_tol = 1e-5
     rel_tol = 1e-4
-    print 'Pass  ' + 'Species'.ljust(16)
+    print('Pass  ' + 'Species'.ljust(16))
     checks = []
-    for key, refvals in spec_ref.iteritems():
+    for key, refvals in spec_ref.items():
         if key in spec_test:
             checkvals = spec_test[key]
             diffs = [checkval - refval for checkval, refval in zip(checkvals, refvals)]
@@ -32,19 +33,19 @@ def check(check_path, test_path):
         if not check:
             beginc = '\033[91m'
 
-        print beginc + str('PASS' if check else 'FAIL').ljust(5), key.ljust(16) + endc
+        print(beginc + str('PASS' if check else 'FAIL').ljust(5), key.ljust(16) + endc)
         if check == False:
             ti = 0
-            print beginc + 'Total , Failed , Pct', len(refvals), ',', len(refvals) - sum(tests), ',', round((1-sum(tests)/float(len(refvals)))*100, 1), '%' + endc
+            print(beginc + 'Total , Failed , Pct', len(refvals), ',', len(refvals) - sum(tests), ',', round((1-sum(tests)/float(len(refvals)))*100, 1), '%' + endc)
             for r, c, d, a, t in zip(refvals, checkvals, diffs, allowables, tests):
                 if not t:
                     ti += 1
-                    print beginc + '  Fail: %s' % ti + endc
-                    print beginc + '  Ref    : %s' % r + endc
-                    print beginc + '  Check  : %s' % c + endc
-                    print beginc + '  Diff   : %s' % d + endc
-                    print beginc + '  Allowed: %s' % copysign(a, d) + endc
-                    print
+                    print(beginc + '  Fail: %s' % ti + endc)
+                    print(beginc + '  Ref    : %s' % r + endc)
+                    print(beginc + '  Check  : %s' % c + endc)
+                    print(beginc + '  Diff   : %s' % d + endc)
+                    print(beginc + '  Allowed: %s' % copysign(a, d) + endc)
+                    print()
     
     sum_checks = sum(checks)
     len_checks = len(checks)
@@ -55,10 +56,10 @@ def check(check_path, test_path):
     
     if failed:
         beginc = '\033[91m'
-    print beginc + '-'*30 + endc
-    print '\033[92m' + 'Passed'.ljust(16) + counts + endc
-    print  beginc + 'Failed'.ljust(16) + ' %5d/%d' % ((len_checks - sum_checks), len_checks) + endc
-    print ''    
+    print(beginc + '-'*30 + endc)
+    print('\033[92m' + 'Passed'.ljust(16) + counts + endc)
+    print( beginc + 'Failed'.ljust(16) + ' %5d/%d' % ((len_checks - sum_checks), len_checks) + endc)
+    print(''    )
     
     return checks
 
