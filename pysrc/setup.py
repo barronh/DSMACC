5cc5e0258e8a07e69297c2ba5b71e3b571b8a420
Barron Henderson <barronh@gmail.com>
2018-01-22 21:12:41 -0500

    Adding conditional update of .pyf files
    This prevents a rebuild when not necessary.

--- pysrc/setup.py
+++ pysrc/setup.py
@@ -3,13 +3,20 @@ from numpy.distutils.core import setup
 from numpy.distutils.core import Extension
 
 def makekpp(mech, path):
-    with open('../{}/src/dsmacc_Parameters.f90'.format(path)) as paramf: 
+    import os.path
+    parampath = '../{}/src/dsmacc_Parameters.f90'.format(path)
+    outpath = '{}.pyf'.format(mech)
+    if os.path.exists(outpath):
+        param_mtime = os.path.getmtime(parampath)
+        out_mtime = os.path.getctime(outpath)
+        if out_mtime > param_mtime: return
+    with open(parampath) as paramf: 
         lines = [l for l in paramf.read().split('\n') if ' NVAR =' in l or ' NSPEC =' in l or ' NREACT =' in l]
     params = {'mech': mech}
     data = '\n'.join(lines).replace('  INTEGER, PARAMETER :: ', '')
     exec(data, None, params)
 
-    with open('kpp.pyf.tmp') as intmp, open('{}.pyf'.format(mech), 'w') as outf: 
+    with open('kpp.pyf.tmp') as intmp, open(outpath, 'w') as outf: 
       outf.write(intmp.read() % params)
 
 _sources = ['../{}/src/dsmacc_Precision.f90', '../{}/src/dsmacc_Parameters.f90', '../{}/src/dsmacc_Function.f90', '../{}/src/dsmacc_Global.f90', '../{}/src/dsmacc_JacobianSP.f90', '../{}/src/dsmacc_Jacobian.f90', '../{}/src/dsmacc_LinearAlgebra.f90', '../{}/src/dsmacc_Rates.f90', '../{}/src/dsmacc_Integrator.f90', '../{}/src/dsmacc_Monitor.f90', '../{}/src/dsmacc_Util.f90', '../{}/src/dsmacc_Initialize.f90', '../{}/src/dsmacc_Main.f90', '../{}/src/dsmacc_StoichiomSP.f90', '../{}/src/dsmacc_Stoichiom.f90', '../{}/src/dsmacc_Model.f90', 'pyint.f90', 'pyglob.f90', 'pyrate.f90', 'pymon.f90']
@@ -22,7 +29,7 @@ def addext(mech, path):
                 library_dirs = ['../tuv_new/', '../UCI_fastJX72e/'],
                 libraries = ['tuv', 'fastjx'],
                 include_dirs = ['.', '../src'],
-                extra_f90_compile_args = ['-cpp', '-O2'])
+                extra_f90_compile_args = ['-cpp', '-O0'])
     return mod
     
 crimod = addext('cri', 'cri')
